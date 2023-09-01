f224095fbc8985319b9c6b3bf99bf171809c1b88
102560056 <102560056@student.swin.edu.au>
2023-09-01 15:04:53 +1000

    grid work

--- wallet-insight/src/components/DetailsTab.jsx
+++ wallet-insight/src/components/DetailsTab.jsx
@@ -1,19 +1,67 @@
-import { Grid } from '@mui/material'
+import { Box, Chip, Grid, Typography } from '@mui/material'
 import React from 'react'
 
-export default function DetailsTab() {
+export default function DetailsTab(props) {
+    // Map props to variables
+
     return (
         <>
-            <Grid container spaceing={0} sx={{
-                columnGap: 1.5,
+            <Box sx ={{
+                width:'100%',
+                height:'100%',
+                display:'grid',
+                gap:1,
+                // gap:'8px',
+                gridTemplateColumns:'1fr 1fr',
+                // gridTemplateRows:'repeat(1fr, 16)',
+                alignItems:'center',
             }}>
-                <Grid item xs={12} sx={{ backgroundColor: 'orange', padding: 0, margin: 0 }}>title</Grid>
-                <Grid item xs={12} sx={{ backgroundColor: 'purple', padding: 0, margin: 0 }}>
-                    <Grid item xs={5.7} sx={{ backgroundColor: 'red', padding: 0, margin: 0 }}>a</Grid>
-                    <Grid item xs={5.7} sx={{ backgroundColor: 'blue', padding: 0, margin: 0 }}>n</Grid>
-                </Grid>
-            </Grid>
-            {/* react grid? */}
+                <Typography variant='h5' noWrap='false' sx={{gridColumn:'span 2'}}>Thy4SDBjSyas22g1EDSydsa</Typography>
+                {/* <Typography variant='h5' noWrap='false' className='wallet-address'>{walletAddress}</Typography> */}
+
+                <Typography variant='h6' sx={{justifySelf:'end', gridColumn:'span 1'}}>Current Balance:</Typography>
+                <Typography variant='h6' sx={{justifySelf:'start', gridColumn:'span 1'}}>185 USDT</Typography>
+
+                
+                <Typography variant='h6' sx={{justifySelf:'end', gridColumn:'span 1'}}>Total Sent:</Typography>
+                <Typography variant='h6' sx={{justifySelf:'start', gridColumn:'span 1'}}>4,794,806.1506 USDT</Typography>
+
+                
+                <Typography variant='h6' sx={{justifySelf:'end', gridColumn:'span 1'}}>Total Recieved:</Typography>
+                <Typography variant='h6' sx={{justifySelf:'start', gridColumn:'span 1'}}>4,794,806.1506 USDT</Typography>
+
+                
+                <Typography variant='h6' sx={{justifySelf:'end', gridColumn:'span 1'}}>To/From Trans:</Typography>
+                <Typography variant='h6' sx={{justifySelf:'start', gridColumn:'span 1'}}>7/123</Typography>
+
+                {/*  */}
+
+                <Chip variant='filled' color='error'>Snt</Chip>
+
+                <Typography variant='h6' sx={{justifySelf:'end', gridColumn:'span 1'}}>Latest Transaction:</Typography>
+                <Typography variant='h6' sx={{justifySelf:'start', gridColumn:'span 1'}}>sduf6hgq2BySDSAh1nicd398hBSGj</Typography>
+
+                <Typography variant='h6' sx={{justifySelf:'end', gridColumn:'span 1'}}>Time:</Typography>
+                <Typography variant='h6' sx={{justifySelf:'start', gridColumn:'span 1'}}>03/11/2023 23:28</Typography>
+
+                <Typography variant='h6' sx={{justifySelf:'end', gridColumn:'span 1'}}>Amount:</Typography>
+                <Typography variant='h6' sx={{justifySelf:'start', gridColumn:'span 1'}}>12.678 USDT</Typography>
+
+                {/*  */}
+
+                <Chip variant='filled' color='success'>Rcv</Chip>
+
+                <Typography variant='h6' sx={{justifySelf:'end', gridColumn:'span 1'}}>First Transaction:</Typography>
+                <Typography variant='h6' sx={{justifySelf:'start', gridColumn:'span 1'}}>0xasdfuy1H2hSiii12YggbUIj1mawdf71hbnutGh5R</Typography>
+
+                <Typography variant='h6' sx={{justifySelf:'end', gridColumn:'span 1'}}>Time:</Typography>
+                <Typography variant='h6' sx={{justifySelf:'start', gridColumn:'span 1'}}>01/05/2018 08:45</Typography>
+
+                <Typography variant='h6' sx={{justifySelf:'end', gridColumn:'span 1'}}>Amount:</Typography>
+                <Typography variant='h6' sx={{justifySelf:'start', gridColumn:'span 1'}}>199.99 USDT</Typography>
+                
+                
+            </Box>
         </>
     )
 }
